4c0cea1f90e12cb1b89cac0079d86eceae78d00c
boomty <114364342+boomty@users.noreply.github.com>
2026-03-25 08:55:41 -0700

    Added Sound Code

--- src/audio_output.cpp
+++ src/audio_output.cpp
@@ -1,38 +1,28 @@
-/**
- * @file streams-memory_raw-i2s.ino
- * @author Phil Schatzmann
- * @brief Compile with Partition Scheme Hughe APP!
- * @version 0.1
- * @date 2021-01-24
- * 
- * @copyright Copyright (c) 2021
- * 
- */
 #include "AudioTools.h"
-#include "StarWars30.h"
+#include "opening.h"
 
-AudioInfo info(22050, 1, 16);
-I2SStream i2s;  // Output to I2S
+uint8_t channels = 2;
+uint16_t sample_rate = 22050;
+uint8_t bits_per_sample = 16;
+
 MemoryStream music(StarWars30_raw, StarWars30_raw_len);
+I2SStream i2s;  // Output to I2S
 StreamCopy copier(i2s, music); // copies sound into i2s
 
 void setup(){
     Serial.begin(115200);
-    AudioToolsLogger.begin(Serial, AudioToolsLogLevel::Info);
-
     auto config = i2s.defaultConfig(TX_MODE);
-    config.pin_bck = 3;
-    config.pin_ws = 4;
-    config.pin_data = 5;
-    // config.pin_mck = 0; // optional master clock pin
-    // config.i2s_format = I2S_STD_FORMAT; // default format
-    // config.is_master = true; // default esp32 is master
+    config.pin_ws = 44;
+    config.pin_bck = 7;
+    config.pin_data = 43;
+    config.sample_rate = sample_rate;
+    config.channels = channels;
+    config.bits_per_sample = bits_per_sample;
     i2s.begin(config);
+
+    music.begin();
 }
 
 void loop(){
-    if (!copier.copy()){
-      i2s.end();
-      stop();
-    }
+    copier.copy();
 }
